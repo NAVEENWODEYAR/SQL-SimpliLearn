8addf8adf37c0e82f2051f43d832da09b3be0ddd
Naveen <naveenkrishnawodeyar@gmail.com>
2024-04-17 20:36:03 +0530

    structure modified

--- src/main/java/com/jdbc/Student.java
+++ src/main/java/com/jdbc/Student.java
@@ -1,10 +1,17 @@
 package com.jdbc;
 
+import java.io.Serializable;
+
 /**
  * 
  * @author Naveen K Wodeyar
  * @date 23-01-2024
  */
-public class Student {
+public class Student implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 	
 }
